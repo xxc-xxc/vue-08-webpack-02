d42de3bc2dde3261135e03993ed862fbc8738bf3
xxc <2331896806@qq.com>
2021-02-04 00:33:59 +0800

    webpack基本使用&webpack加载器loader使用

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import $ from 'jquery'
+import "./css/1.css"
+import "./css/1.less"
+import "./css/1.scss"
+
 $(function(){
     $("li:odd").css("background","cyan");
     $("li:even").css("background","red");
 })
+
+class Person {
+    static name = "xxc";
+}
+
+console.log(Person.name);
--- webpack.config.js
+++ webpack.config.js
@@ -12,5 +12,17 @@ module.exports = {
         path: path.join(__dirname, './dist'), // 输出文件的存放路径
         filename: 'bundle.js' // 输出文件的名称
     },
-    plugins: [htmlPlugin] // plugins 数组是 webpack 打包期间会用到的一些插件列表
+    plugins: [htmlPlugin], // plugins 数组是 webpack 打包期间会用到的一些插件列表
+    module: {
+        rules: [
+            { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+            { test: /\.less$/, use: ['style-loader', 'css-loader', 'less-loader'] },
+            { test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader'] },
+            {
+                test: /\.jpg|png|gif|bmp|ttf|eot|svg|woff|woff2|jpeg$/,
+                use: 'url-loader?limit=16940'
+            },
+            { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ }
+        ]
+    }
 }
