a06f0b38b8535565f2d47c7e755a0e25864fdf8e
xxc <2331896806@qq.com>
2021-02-04 20:50:29 +0800

    webpack中使用vue

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import $ from 'jquery'
 import "./css/1.css"
 import "./css/1.less"
 import "./css/1.scss"
+// 1.导入Vue构造函数
+import Vue from 'vue'
+// 2.导入APP根组件
+import App from './components/App.vue'
+
 
 $(function(){
     $("li:odd").css("background","cyan");
@@ -13,3 +18,10 @@ class Person {
 }
 
 console.log(Person.name);
+
+const vm = new Vue({
+    // 3.指定vm实例要控制的页面区域
+    el: '#app',
+    // 4.通过render函数，把指定的组件渲染到el区域中
+    render: h => h(App),
+})
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const htmlPlugin = new HtmlWebpackPlugin({
     template: "./src/index.html",
     filename: "index.html"
-})
+});
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
     mode: "development", //可以设置为development(开发模式)，production(发布模式)
@@ -12,7 +13,10 @@ module.exports = {
         path: path.join(__dirname, './dist'), // 输出文件的存放路径
         filename: 'bundle.js' // 输出文件的名称
     },
-    plugins: [htmlPlugin], // plugins 数组是 webpack 打包期间会用到的一些插件列表
+    plugins: [
+        htmlPlugin,// plugins 数组是 webpack 打包期间会用到的一些插件列表
+        new VueLoaderPlugin()
+    ], 
     module: {
         rules: [
             { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
@@ -22,7 +26,8 @@ module.exports = {
                 test: /\.jpg|png|gif|bmp|ttf|eot|svg|woff|woff2|jpeg$/,
                 use: 'url-loader?limit=16940'
             },
-            { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ }
+            { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ },
+            { test: /\.vue$/, loader: 'vue-loader' }
         ]
     }
 }
